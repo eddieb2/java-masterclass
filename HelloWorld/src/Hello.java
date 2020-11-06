bbabd93ffea558ee48c6d3aa96ac705eecc29e2c
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-06 13:47:40 -0500

    22.variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = (5 + 10 + (2 * 10));
+
+        // sout
+        System.out.println(myFirstNumber);
     }
 }
