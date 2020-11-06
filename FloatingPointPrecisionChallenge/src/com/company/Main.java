6aac7d7195f9ae9532e8761c0ba805655813d579
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-06 16:02:02 -0500

    29. Floating point precision challenge

---
+++ FloatingPointPrecisionChallenge/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double pounds = 10d;
+	    double kilogram = 0.45359237d;
+	    double result = pounds * kilogram;
+
+        System.out.println(result);
+    }
+}
