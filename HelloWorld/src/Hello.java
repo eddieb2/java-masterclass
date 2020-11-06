56b080805512c5bf1ed41aa7b6d6ccff6c6eeeb4
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-06 13:54:48 -0500

    23.starting out with expressions

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,8 +4,14 @@ public class Hello {
         System.out.println("Hello World");
 
         int myFirstNumber = (5 + 10 + (2 * 10));
-
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
         // sout
+
         System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
 }
