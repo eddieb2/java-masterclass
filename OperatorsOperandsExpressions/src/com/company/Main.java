d5446047f0368aee45fa18bbbd4fa714109d036a
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-07 18:41:48 -0500

    35. logical AND operator

--- OperatorsOperandsExpressions/src/com/company/Main.java
+++ OperatorsOperandsExpressions/src/com/company/Main.java
@@ -65,5 +65,19 @@ public class Main {
         if (isAlien == false) {
             System.out.println("It is not an alien!");
         }
+
+        // ---- Logical AND Operator ----
+
+        int topScore = 80;
+
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100.");
+        }
     }
 }
