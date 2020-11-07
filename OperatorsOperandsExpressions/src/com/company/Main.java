4ed77a405e01e9db0321f02f42d1b2487a44cfaf
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-07 18:21:25 -0500

    33. abbreviating operators

--- OperatorsOperandsExpressions/src/com/company/Main.java
+++ OperatorsOperandsExpressions/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+        // Operators and Operands
+        // + - / * %
         int result = 1 + 2; // 1 + 2 = 3
         System.out.println(result);
 
@@ -21,5 +22,30 @@ public class Main {
 
         result = result % 3;
         System.out.println("4 % 3 = " + result);
+
+        // Abbreviating Operators
+
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("Result++ = " + result);
+
+        result--; // 2 - 1 = 1;
+        System.out.println("Result-- = " + result);
+
+        // result = result + 2
+        result += 2;
+        System.out.println("result += 2 =" + result);
+
+        // result = result * 10
+        result *= 10;
+        System.out.println("result *= 10 =" + result);
+
+        // result = result / 3;
+        result /= 3;
+        System.out.println("result /= 3 =" + result);
+
+        // result = result - 2;
+        result -= 2;
+        System.out.println("result -= 2 =" + result);
     }
 }
