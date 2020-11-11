0d1792d21ea8310f3b070d519d019d8001971ef8
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-11 15:42:07 -0500

    48. More on methods and a challenege

--- MethodsInJava/src/com/company/Main.java
+++ MethodsInJava/src/com/company/Main.java
@@ -3,18 +3,41 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        calculateScore(true, 800,5,100);
-        calculateScore(true, 10000,8,200);
+
+        int highScore = calculateScore(true, 800,5,100);
+        System.out.println("Your final score was " + highScore);
+
+        highScore = calculateScore(true, 10000,8,200);
+        System.out.println("Your final score was " + highScore);
+
+        int position =  calculateHighScorePosition(highScore);
+        displayHighScorePosition("Eddie", position);
+
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         } else {
             return -1;
+        }
+    }
+
+    public static void displayHighScorePosition(String player, int position) {
+        System.out.println(player + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if (score >= 1000){
+            return 1;
+        } else if (score >= 500 && score < 1000) {
+            return 2;
+        } else if (score >= 100 && score < 500) {
+            return 3;
+        } else {
+            return 4;
         }
     }
 }
