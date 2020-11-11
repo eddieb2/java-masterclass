7d00919db5a9c627a2a59730dbebfff412ce3964
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-11 17:38:57 -0500

    coding ex 2: MegaBytes Converter

---
+++ MegaBytesConverter/src/com/company/MegaBytesConverter.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        int megabytes = kiloBytes / 1024;
+        int remaining = kiloBytes % 1024;
+
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB" + " and " + remaining + " KB");
+        }
+
+    }
+
+}
