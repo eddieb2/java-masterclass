0d39f6b5af353caa87d0b2d2f3cbe17e1e0b321b
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-12 20:18:00 -0500

    60. bonus challenge solution

--- SecondMinutesChallenge/src/com/company/Main.java
+++ SecondMinutesChallenge/src/com/company/Main.java
@@ -1,25 +1,46 @@
 package com.company;
 
 public class Main {
+    //  CONSTANT - cannot be changed
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
 
     public static void main(String[] args) {
         System.out.println(getDurationString(65L, 45L));
         System.out.println(getDurationString(60L));
+        System.out.println(getDurationString(-41L));
+        System.out.println(getDurationString(65L,9L));
     }
 
     private static String getDurationString(long minutes, long seconds) {
         if ((minutes < 0) || (seconds < 0) || (seconds > 59)) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if (hours < 10){
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10){
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if (seconds < 10){
+            secondsString = "0" + secondsString;
+        }
+
+        return hoursString + " " + minutesString + " " + secondsString + " ";
+//        return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
 
     private static String getDurationString(long seconds) {
         if (seconds < 0) {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long minutes = seconds / 60;
@@ -28,3 +49,4 @@ public class Main {
         return getDurationString(minutes, remainingSeconds);
     }
 }
+
