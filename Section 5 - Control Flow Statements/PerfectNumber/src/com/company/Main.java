8daac972b1485702906c738cb4be20f2a0d4e36e
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-14 15:11:05 -0500

    coding ex 22: perfect number

---
+++ Section 5 - Control Flow Statements/PerfectNumber/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int total = 0;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+        if (total == number) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
