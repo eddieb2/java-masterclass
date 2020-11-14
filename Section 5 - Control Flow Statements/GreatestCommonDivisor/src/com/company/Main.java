6be666022ebfeae94fe3113f90365acb28bef133
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-14 12:37:53 -0500

    coding ex 20: greatest common divisor

---
+++ Section 5 - Control Flow Statements/GreatestCommonDivisor/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getGreatestCommonDivisor(25,15));
+        System.out.println(getGreatestCommonDivisor(12,30));
+        System.out.println(getGreatestCommonDivisor(9,18));
+        System.out.println(getGreatestCommonDivisor(81,153));
+
+    }
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int greatestCommonDivisor = 0;
+
+        if (first > second) {
+            for (int i = 1; i <= first; i++) {
+                if (first % i == 0 && second % i == 0) {
+                    greatestCommonDivisor = i;
+                }
+
+            }
+        } else {
+            for (int i = 1; i <= second; i++) {
+                if (first % i == 0 && second % i == 0) {
+                    greatestCommonDivisor = i;
+                }
+            }
+        }
+
+        return greatestCommonDivisor;
+    }
+}
