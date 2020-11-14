492308aa177353dd7503c0b43e57471e822a87f9
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-14 10:43:50 -0500

    adds second solution

--- Section 5 - Control Flow Statements/FirstAndLastDigitSum/src/com/company/Main.java
+++ Section 5 - Control Flow Statements/FirstAndLastDigitSum/src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Final Sum: " + sumFirstAndLastDigit(10002));
+        System.out.println("Final Sum: " + secondSolutionSumFirstAndLastDigit(101));
     }
 
     public static int sumFirstAndLastDigit(int number) {
@@ -43,5 +44,21 @@ public class Main {
 
         // return sum of first and last digit
         return sum;
+    }
+
+    public static int secondSolutionSumFirstAndLastDigit(int number) {
+        if (number < 0){
+            return -1;
+        }
+
+
+        int lastDigit = number % 10;
+        int firstNumber = number;
+
+        while (number >= 10) {
+            firstNumber = number /= 10;
+        }
+
+        return firstNumber + lastDigit;
     }
 }
