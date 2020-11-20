ace627917356a1d4eadf303ef09b1194910faf84
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-20 13:47:12 -0500

    coding ex 36: pool area

---
+++ Section 6 - Classes, Constructors and Inheritance/Pool/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Rectangle rectangle = new Rectangle(5,10);
+        System.out.println(rectangle.getWidth());
+        System.out.println(rectangle.getLength());
+        System.out.println(rectangle.getArea());
+
+        System.out.println();
+
+        Cuboid cuboid = new Cuboid(5,10,5);
+        System.out.println(cuboid.getWidth());
+        System.out.println(cuboid.getLength());
+        System.out.println(cuboid.getArea());
+        System.out.println(cuboid.getHeight());
+        System.out.println(cuboid.getVolume());
+    }
+}
---
+++ Section 6 - Classes, Constructors and Inheritance/Pool/src/com/company/Rectangle.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length){
+        if (width < 0 && length < 0) {
+            this.width = 0;
+            this.length = 0;
+        } else if (width < 0) {
+            this.width = 0;
+            this.length = length;
+        } else if (length < 0) {
+            this.length = 0;
+            this.width = width;
+        } else {
+            this.length = length;
+            this.width = width;
+        }
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+}
