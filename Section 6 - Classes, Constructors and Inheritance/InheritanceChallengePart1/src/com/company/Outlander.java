90410f071e38634b0217e8de09318f63aa3f973a
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-19 19:49:43 -0500

    inheritance challenge part 2

---
+++ Section 6 - Classes, Constructors and Inheritance/InheritanceChallengePart1/src/com/company/Outlander.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Outlander extends Car {
+    private int roadServiceMonths;
+
+    public Outlander(int roadServiceMonths) {
+        super("Outlander", "4WD", 4, 4, 6, true);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0) {
+            stop();
+        } else if (newVelocity > 0 && newVelocity <= 10){
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        }
+
+        if (newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+}
