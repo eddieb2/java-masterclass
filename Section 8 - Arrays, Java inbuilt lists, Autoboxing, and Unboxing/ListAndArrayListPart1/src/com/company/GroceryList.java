d855bcc6ac7f604516def3ea74df2eb275ae5c17
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-27 20:32:34 -0500

    105

--- Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/ListAndArrayListPart1/src/com/company/GroceryList.java
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/ListAndArrayListPart1/src/com/company/GroceryList.java
@@ -28,4 +28,17 @@ public class GroceryList {
 
         System.out.println(theItem + " was removed from the list.");
     }
+
+    public String findItem(String searchItem) {
+//        boolean exists = groceryList.contains(searchItem);
+
+        int position = groceryList.indexOf(searchItem);
+
+        if (position >= 0) {
+            return groceryList.get(position);
+        }
+
+        return null;
+    }
+
 }
