9cda544d1161cfd2ee9b510ed68f6eb1b45c2c94
Edward Blanciak <blanciak.edward2@gmail.com>
2020-11-28 16:15:57 -0500

    107. ArrayList Challenge Part 1

---
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/ArrayListChallenge/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    /*
+        Create a program that implements a simple mobile phone with the folowing capabilities
+
+        able to add, modify, remove and query contact names - mobile phone
+        separate class for Contacts
+        create a master class named MobilePhone that holds the ArrayList of Contacts
+        add a menu of options that are available
+        options: quit, print list of contacts, add contact, update contact, remove contact, search contact
+
+    */
+
+//    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//	    MobilePhone mobilePhone = new MobilePhone("111-111-1111");
+
+//	    mobilePhone.addContact("Eddie", "123-456-7890");
+//	    mobilePhone.getContacts();
+    }
+}
---
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/ArrayListChallenge/src/com/company/MobilePhone.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> contacts;
+
+    public MobilePhone(String myNumber){
+        this.myNumber = myNumber;
+        this.contacts = new ArrayList<Contact>();
+    }
+
+    public String getNumber() {
+        return this.myNumber;
+    }
+
+    // add contact
+    public boolean addContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file.");
+            return false;
+        } else {
+            contacts.add(contact);
+            System.out.println("Contact was successfully added!");
+            return true;
+        }
+    }
+
+    // update contact
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int foundPosition = findContact(oldContact);
+
+        if (foundPosition < 0) {
+            System.out.println(oldContact.getName() + ", was not found.");
+            return false;
+        } else {
+            this.contacts.set(foundPosition, newContact);
+            System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+            return true;
+        }
+    }
+
+    // find contact
+    private int findContact(Contact contact) {
+        return this.contacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < this.contacts.size(); i++) {
+            Contact contact = this.contacts.get(i);
+            if (contact.getName().equals(contactName)){
+                return i;
+            }
+        }
+
+        return -1;
+    }\
+
+    // get contacts
+    public void getContacts() {
+        for (int i = 0; i < contacts.size(); i++) {
+            System.out.println("#" + i+1 + " Name: " + contacts.get(i).getName() + " | Number: " + contacts.get(i).getNumber());
+        }
+    }
+}
