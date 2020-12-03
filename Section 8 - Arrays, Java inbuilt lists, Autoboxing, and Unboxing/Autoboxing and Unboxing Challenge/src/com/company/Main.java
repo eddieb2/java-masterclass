4392925f93d5102b12ebe038336961ae83bad88a
Edward Blanciak <blanciak.edward2@gmail.com>
2020-12-03 14:20:45 -0500

    part 3

--- Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/Autoboxing and Unboxing Challenge/src/com/company/Main.java
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/Autoboxing and Unboxing Challenge/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        Bank bank = new Bank("Bank1");
+        bank.addBranch("Branch1");
+        bank.addCustomer("Branch1", "eddie", 100.00);
 
+        bank.addBranch("Branch2");
+        bank.addCustomer("Branch2", "Joe", 200.00);
+
+        bank.addCustomerTransaction("Branch1", "eddie", 50.00);
+
+        bank.listCustomers("Branch1", true);
     }
 }
