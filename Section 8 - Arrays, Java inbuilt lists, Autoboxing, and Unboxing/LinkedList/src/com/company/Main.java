9fd729df8606a7e17c107ecb9b27d9d7196dfd57
Edward Blanciak <blanciak.edward2@gmail.com>
2020-12-09 17:54:51 -0500

    117. linked list part 1

--- Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/LinkedList/src/com/company/Main.java
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/LinkedList/src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	    Customer customer = new Customer("eddie", 60.00);
+	    Customer anotherCustomer;
+
+	    // anotherCustomer is pointing to customer and NOT creating a new class.
+	    anotherCustomer = customer;
+	    anotherCustomer.setBalance(12.00);
+
+//        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println(i + ": " + intList.get(i));
+		}
+
+		intList.add(1,2);
+
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println(i + ": " + intList.get(i));
+		}
+	}
 }
