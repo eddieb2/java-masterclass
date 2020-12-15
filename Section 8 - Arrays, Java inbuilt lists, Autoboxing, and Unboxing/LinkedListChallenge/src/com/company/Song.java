a1fda6acc8f5ba09cb38acf040e59301fac7b15c
Edward Blanciak <blanciak.edward2@gmail.com>
2020-12-14 19:30:22 -0500

    119. LinkedList Challenge Part 1

---
+++ Section 8 - Arrays, Java inbuilt lists, Autoboxing, and Unboxing/LinkedListChallenge/src/com/company/Song.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
